bee426b3d58681a4ab5f38bd558d44bd9fd13e29
sandycodes-1502 <torkadsandesh@gmail.com>
2025-10-29 10:30:44 +0530

    FIbonacci Series Recursive

---
+++ C/Fibonacii_using_recursive_function.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Function to calculate Fibonacci number using recursion
+int fibonacci(int num) {
+    if (num <= 0)
+        return 0;
+    else if (num == 1)
+        return 1;
+    else
+        return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    if (scanf("%d", &n) != 1) {
+        fprintf(stderr, "Invalid input\n");
+        return 1;
+    }
+
+    int result = fibonacci(n);
+    printf("Fibonacci number at position %d is %d\n", n, result);
+
+    return 0;
+}
