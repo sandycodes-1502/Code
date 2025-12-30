6c1dc9655c562dfdd3f8ceb185bd0f6b04b7a9d2
sandycodes-1502 <torkadsandesh@gmail.com>
2025-12-30 12:43:11 +0530

    Regular Commit

--- MyApp/app/src/main/java/com/sandycodes/myapp/activities/MainActivity.kt
+++ MyApp/app/src/main/java/com/sandycodes/myapp/activities/MainActivity.kt
@@ -1,8 +1,8 @@
-package com.sandycodes.myapp
+package com.sandycodes.myapp.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import com.sandycodes.myapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ MyApp/app/src/main/java/com/sandycodes/myapp/fragment/BlankFragment.kt
@@ -0,0 +1,19 @@
+package com.sandycodes.myapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sandycodes.myapp.R
+
+class BlankFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+}
